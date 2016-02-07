b0d013128f7466cc9757a36a7173c1b8c0b7486c
Lenovo <Lenovo@Lenovo-PC>
2016-02-07 18:11:21 +0100

    voir

---
+++ CdkmTestJava/src/com/cdkm/tri/Liro.java
@@ -0,0 +1,10 @@
+package com.cdkm.tri;
+
+public class Liro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CdkmTestJava/src/com/cdkm/tri/TriBull.java
+++ CdkmTestJava/src/com/cdkm/tri/TriBull.java
@@ -3,7 +3,8 @@ package com.cdkm.tri;
 public class TriBull {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//TODO Auto-generated method stub
+		System.out.println("test Joyce sere");
 		System.out.println("test Joyce sere");
 	}
 
